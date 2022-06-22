bc2a100d116fd1f5067687c49a0bc37996724c71
Nate <78760239+NateNoNameSOFT@users.noreply.github.com>
2022-06-21 23:25:30 -0700

    comment/document all player and sprite code

--- feiht-god/src/player.rs
+++ feiht-god/src/player.rs
@@ -21,9 +21,16 @@ impl Plugin for PlayerPlugin {
 }
 
 fn camera_follow(
+    // query for entitys with Transform and Player component this is not in a tuple because 
+    // the Player component data does not need to be acessed, we just need to query for an 
+    // entity that has the transform component (The data we will acess) and the player component,
+    // (data we do not need to access)
     player_query: Query<&Transform, With<Player>>,
+    // query for entitys with a Transform and Camera component but skip entitys with a 
+    // player component because the player component could have a camera component
     mut camera_query: Query<&mut Transform, (Without<Player>, With<Camera>)>
 ) {
+    // get transforms from querys with single (since only one entity matches the querys)
     let player_transform = player_query.single();
     let mut camera_transform = camera_query.single_mut();
 
@@ -31,19 +38,23 @@ fn camera_follow(
     camera_transform.translation.y = player_transform.translation.y;
 }
 
-// query for player, look for entitys that have Player component and transform component,
-// we also want keyboard input in this system so we will get the Input resource of type KeyCode
 fn player_movement(
+    // query for the player, the players transform will need to be adjusted so it is a mutable reference 
     mut player_query: Query<(&Player, &mut Transform)>, 
+    // query for walls with colliders, we will need the transform of the walls 
+    // (again without player is required because the Player component could have a TileCollider component, meaning more than one result/entity)
     wall_query: Query<&Transform, (With<TileCollider>, Without<Player>)>,
+    // we will also need a keyboard input here so we will get the Input resource of type KeyCode
     keyboard: Res<Input<KeyCode>>, 
+    // we will use the Time resource to multiply by delta time
     time: Res<Time>
 ) {
-    // get the transform out of the query
+    // get the transform and player component out of the query
     let (player, mut transform) = player_query.single_mut();
 
     // (We can now check for keyboard input and edit the transform since we have a mutable reference to it)
 
+    // add/subtract any movement from keypresses on the y axis
     let mut y_delta = 0.0;
     if keyboard.pressed(KeyCode::W) {
         y_delta += player.speed * TILE_SIZE * time.delta_seconds();
@@ -52,6 +63,7 @@ fn player_movement(
         y_delta -= player.speed * TILE_SIZE * time.delta_seconds();
     }
 
+    // add/subtract any movement from keypresses on the x axis
     let mut x_delta = 0.0;
     if keyboard.pressed(KeyCode::D) {
         x_delta += player.speed * TILE_SIZE * time.delta_seconds();
@@ -60,42 +72,66 @@ fn player_movement(
         x_delta -= player.speed * TILE_SIZE * time.delta_seconds();
     }
 
+    // the target as in where the player should be from the pressed buttons
     let target = transform.translation + Vec3::new(x_delta, 0.0, 0.0);
-    if wall_collision_check(target, &wall_query) {
+    // if there is no collision between the player and a wall ... 
+    if !wall_collision_check(target, &wall_query) {
+        // update the players position to what they pressed
         transform.translation = target;
     }
 
+    // the target as in where the player should be from the pressed buttons
     let target = transform.translation + Vec3::new(0.0, y_delta, 0.0);
-    if wall_collision_check(target, &wall_query) {
+    // if there is no collision between the player and a wall ...
+    if !wall_collision_check(target, &wall_query) {
+        // update the players position to what they pressed
         transform.translation = target;
     }
 }
 
 fn wall_collision_check(
+    // where the player should be from the key presses
     target_player_pos: Vec3,
+    // query for all transforms with a TileCollider component
     wall_query: &Query<&Transform, (With<TileCollider>, Without<Player>)>
+    // true -> collided, false -> no collision
 ) -> bool {
+    // iterate through each wall transform
     for wall_transform in wall_query.iter() {
+        // create a collision "box"
         let collision = collide(
+            // center position of player collision rectangle
             target_player_pos,
+            // dimensions of player collision rectangle
             Vec2::splat(TILE_SIZE * 0.9),
+
+            // center postion of wall collision rectangle
             wall_transform.translation,
+            // dimensions of wall collision rectangle
             Vec2::splat(TILE_SIZE)
         );
+        // if there is collision of any value return true (as in collision occured)
         if collision.is_some() {
-            return false;
+            return true;
         }
     }
 
-    true
+    // no tiles returned collision of any intensity
+    false
 }
 
-// We need commands because we will be spawning an entity,
-// and the spritesSheet resource will be used to get the graphics handle
-fn spawn_player(mut commands: Commands, sprites: Res<SpriteSheet>) {
+fn spawn_player(
+    // we need commands because we will be spawning a entity (inside of spawn_sprite),
+    // we will also need commands because we will be adding components to our player entity
+    mut commands: Commands, 
+    // we will need the sprite sheet resource because we will be loading sprites
+    sprites: Res<SpriteSheet>
+) {
+    // create a new player entity
     let player = spawn_sprite(
         &mut commands, 
         &sprites,
+        // index 1 contains forward facing player sprite
         1,
         Vec3::new(2.0 * TILE_SIZE, -2.0 * TILE_SIZE, 900.0)
     );
@@ -105,17 +141,17 @@ fn spawn_player(mut commands: Commands, sprites: Res<SpriteSheet>) {
         .insert(Name::new("Player"))
         .insert(Player { speed: 3.0 });
 
-    let background = spawn_sprite(
-        &mut commands, 
-        &sprites, 
-        0,  
-        Vec3::new(0.0, 0.0, -1.0)
-    );
+    // let background = spawn_sprite(
+    //     &mut commands, 
+    //     &sprites, 
+    //     0,  
+    //     Vec3::new(0.0, 0.0, -1.0)
+    // );
 
-    commands.entity(background)
-        // add components to background entity
-        .insert(Name::new("Background"));
+    // commands.entity(background)
+    //     // add components to background entity
+    //     .insert(Name::new("Background"));
 
-    // set the background to be a child of the player
-    commands.entity(player).push_children(&[background]);
+    // // set the background to be a child of the player
+    // commands.entity(player).push_children(&[background]);
 }
--- feiht-god/src/sprites.rs
+++ feiht-god/src/sprites.rs
@@ -10,36 +10,56 @@ pub struct SpriteSheet(Handle<TextureAtlas>);
 impl Plugin for SpritePlugin {
     fn build(&self, app: &mut App) {
         app
+            // we want this sprite sheet to be the first thing loaded so nothing trys
+            // to acess a sprite while they are still not fully loaded
             .add_startup_system_to_stage(StartupStage::PreStartup, load_sprites);
     }
 }
 
 pub fn spawn_sprite(
+    // to spawn the sprite
     commands: &mut Commands,
+    // to get the single sprite, and needed to spawn a sprite bundle
     sprites: &SpriteSheet,
+    // which sprite to draw from the sheet
     index: usize,
+    // position of the entity
     translation: Vec3
 ) -> Entity {
-        // create a texture atlas sprite, and set the index to 1 (the smile face on the sheet)
+        // create a new texture atlas sprite and set it to the index provided
         let mut sprite = TextureAtlasSprite::new(index);
+        // set the size to the standard tile size
         sprite.custom_size = Some(Vec2::splat(TILE_SIZE));
     
         commands
+            // spawn a new entity with the components contained 
+            // in a bundle (a bundle is a collection of components)
+            // We will use the SpriteSheetBundle than contains components for drawing
+            // a single sprite from the texture atlas (sprite sheet)
             .spawn_bundle(SpriteSheetBundle {
+                // single sprite from the texture atlas to be drawn
                 sprite,
+                // handle to the texture atlas that holds all sprites
                 texture_atlas: sprites.0.clone(),
+                // transformation that will be where the sprite is drawn
                 transform: Transform { 
-                translation: translation,
+                    translation: translation,
+                    ..Default::default()
+                },
                 ..Default::default()
-            },
-            ..Default::default()
-        }).id()
+            }
+        ).id() // return id of the newly created [sprite] entity
 }
-
-// Needs commands because we will be adding a resource, 
-// also needs to acess the asset server resource to load the image (from the default plugins)
-// and needs mutable acess to the texture atlas asset manager
-fn load_sprites(mut commands: Commands, assets: Res<AssetServer>, mut texture_atlases: ResMut<Assets<TextureAtlas>>) {
+ 
+fn load_sprites(
+    // commands are needed here because we will be inserting a resource
+    mut commands: Commands, 
+    // we also need the asset server resource for loading the image (sprite sheet)
+    assets: Res<AssetServer>,
+    // we also need a mutable reference to the TextureAtlas asset manager 
+    // because we will be adding an asset to the collection
+    mut texture_atlases: ResMut<Assets<TextureAtlas>>
+) {
     let image = assets.load("Sprites.png");
     // create a texture atlas since it is a sprite sheet
     // the sprites sprite sheet is also padded so we will use from_grid_with_padding
@@ -51,7 +71,10 @@ fn load_sprites(mut commands: Commands, assets: Res<AssetServer>, mut texture_at
         Vec2::splat(1.0)
     );
 
-    // add atlas to texture atlases resource
+    // create a handle to our new TextureAtlas (sprite sheet) named atlas_handle
     let atlas_handle = texture_atlases.add(atlas);
+    // insert a resource that contains our handle, think of a handle as a pointer,
+    // in this case its pointing to our atlas asset that we added to the collection
+    // (This is for simplicity)
     commands.insert_resource(SpriteSheet(atlas_handle));
 }
