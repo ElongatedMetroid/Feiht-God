cfea4c39c8484dd71e5d0aec647b88490dee18fb
Nate <78760239+NateNoNameSOFT@users.noreply.github.com>
2022-06-24 23:15:28 -0700

    update debug menu

--- feiht-god/src/debug.rs
+++ feiht-god/src/debug.rs
@@ -1,7 +1,7 @@
 use bevy::prelude::*;
 use bevy_inspector_egui::{WorldInspectorPlugin, RegisterInspectable};
 
-use crate::{player::Player, sprites::Facing};
+use crate::{player::{Player, EncounterTracker}, sprites::{Facing, AnimationTimer}};
 
 pub struct DebugPlugin;
 
@@ -9,6 +9,8 @@ impl Plugin for DebugPlugin {
     fn build(&self, app: &mut App) {
         if cfg!(debug_assertions) {
             app.add_plugin(WorldInspectorPlugin::new())
+                .register_type::<EncounterTracker>()
+                .register_type::<AnimationTimer>()
                 .register_inspectable::<Player>()
                 .register_inspectable::<Facing>();
         }
